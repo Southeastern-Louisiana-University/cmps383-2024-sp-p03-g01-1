62b49be9443799fb2501b358f0670f6f5f8374d6
Robin White <104403739+TheyCallMeRobin@users.noreply.github.com>
2024-03-13 02:32:49 -0500

    Front-End Landing Page Creation

--- Selu383.SP24.Web/src/App.tsx
+++ Selu383.SP24.Web/src/App.tsx
@@ -1,12 +1,49 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css'
 import Navbar from './Components/Navbar';
+import image1 from './images/Baton Rouge.jpg';
+import image2 from './images/FQNOLA.jpg';
+import image3 from './images/SLCNOLA.jpg';
 
 function App() {
   return (
     <>     
       <Navbar />
+      <div>
+        <p className="landing-p-custom">Where Convenience</p>
+        <p className="landing-p-custom">Checks In</p>
+      </div>
+      <div className="row">
+        <div className="card card-custom">
+          <img className="card-img-top" src={image1} alt="Baton Rouge" />
+          <div className="card-body">
+            <h4 className="card-title">Baton Rouge</h4>
+            <p className="card-text card-text-custom">200 Convention St.</p>
+            <p className="card-text card-text-custom">Baton Rouge, LA 70801</p>
+            <a href="#" className="btn btn-custom">Availability</a>
+          </div> 
+        </div>
+        <div className="card card-custom">
+          <img className="card-img-top" src={image2} alt="FQ NOLA" />
+          <div className="card-body">
+            <h4 className="card-title">French Quarter</h4>
+            <p className="card-text card-text-custom">225 Barrone St.</p>
+            <p className="card-text card-text-custom">New Orleans, LA 70112</p>
+            <a href="#" className="btn btn-custom">Availability</a>
+          </div> 
+        </div>
+        <div className="card card-custom">
+          <img className="card-img-top" src={image3} alt="SCL NOLA" />
+          <div className="card-body">
+            <h4 className="card-title">Jackson Square</h4>
+            <p className="card-text card-text-custom">405 Esplanade Ave.</p>
+            <p className="card-text card-text-custom">New Orleans, LA 70116</p>
+            <a href="#" className="btn btn-custom">Availability</a>
+          </div> 
+        </div>
+      </div>
     </>
+   
   )
 }
 
