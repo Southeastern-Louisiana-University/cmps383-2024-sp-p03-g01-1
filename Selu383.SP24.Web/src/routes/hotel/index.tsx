2fdcaa3061d8eaf42352a4ae1c72fa1d4aee9cb7
Alexander C <eachristian335@gmail.com>
2024-03-26 19:56:11 -0500

    landing page modifications, error handling, routes, see the below message
    things i changed:
    - we have a routes under main.tsx, add them however you want by just copying and pasting
    - some of the files have been changed, mainly:
    app.tsx:
    - now being used as the nav bar/search bar for each page. the landing page with the cards has been moved to home.tsx
     - we can either: change it to where this gets generated on the home page, which would be just exporting HotelDetails onto App.tsx, or whatever y'all think
    
    home.tsx
    the landing page, currently has static data coded in. can be updated to have an export of HotelDetails or whatever y'all see fit.
    routes>hotel
    includes everything i was working on
    
    error-page.tsx
    self explanatory, can be modified if we want to show anything special or anything like that.
    works when someone navigates outside of an existing URL or breaks shit.
    
    hotelindex.css
    - typed specifically to match hotel>index.tsx
    noticed some interference with the home.tsx so be careful of not renaming your classes
    
    thoughts and future plans:
    onclick of new orleans, baton rouge then search:
    generate an index page that only maps the dtos that include address within the address "new orleans" or "baton rouge"
    
    back end integration so we can have actual modifiable/live data in the index
    
    update the index so it's pretty and has some coherence
    
    general navigation of changes:
    homepage:
    https://localhost:7116/home
    
    index:
    https://localhost:7116/hotels/index
    
    details:
    https://localhost:7116/hotels/details/1-4 currently
    - needs to be modified from backend to have correct naming conventions etc.

---
+++ Selu383.SP24.Web/src/routes/hotel/index.tsx
@@ -0,0 +1,41 @@
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { HotelDto } from "../../Components/HotelDto";
+import "./hotelindex.css";
+
+function Hotels() {
+    const [hotels, setHotels] = useState<HotelDto[]>([]);
+
+    useEffect(() => {
+        fetch("/api/hotels", {
+            method: "get",
+        })
+            .then<HotelDto[]>((r) => r.json())
+            .then((j) => {
+                setHotels(j);
+            });
+    }, []);
+
+    return (
+        <div className="containerindex">
+            <div className="rowindex">
+                {hotels.map((hotel) => (
+                    <div key={hotel.id} className="col-3index">
+                        <div className="card card-customindex">
+                            <img className="card-img-topindex" src={hotel.image} alt={hotel.name} />
+                            <div className="card-bodyindex">
+                                <h4 className="card-titleindex">{hotel.name}</h4>
+                                <p className="card-text card-text-customindex">{hotel.address}</p>
+                                <Link to={`/hotels/details/${hotel.id}`}>
+                                    <button className="btn btn-custom rounded-5index">Availability</button>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Hotels;
