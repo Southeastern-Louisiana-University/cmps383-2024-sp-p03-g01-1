fcdff0809838959cff63e73b6ec79fc36c1137a3
Bradford Parks <bradford.parks@selu.edu>
2024-04-16 23:02:49 -0500

    seeded description works now

--- Selu383.SP24.Mobile/features/details.js
+++ Selu383.SP24.Mobile/features/details.js
@@ -3,33 +3,47 @@ import { View, Text, Button, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native'; 
 import { useAuth } from './AuthContext';
 import { Card, Avatar, IconButton } from 'react-native-paper';
+import seededHotels from './seededHotels';
 
 function DetailsScreen({ route }) {
   const navigation = useNavigation(); 
   const { hotel } = route.params;
 
   const { user } = useAuth();
+  
+  const hotelDetails = seededHotels.find(item => {
+    //console.log("hotel.id:", hotel.id);
+    //console.log("item.id:", item.id);
+    return item.id === hotel.id;
+  });  
+  const description = hotelDetails ? hotelDetails.description : '';
 
 
   const handleBookNow = () => {
     navigation.navigate('Booking', { hotel });
   };
+  // console.log(hotel);
+  // console.log(seededHotels);
+  // console.log("hotelDetails:", hotelDetails);
+  // console.log("description:", description);
+
 
   return (
     <View style={style.container}>
 
       <Card>
       <Card.Title
-    title="Card Title"
-    subtitle="Card Subtitle"
-    left={(props) => <Avatar.Icon {...props} icon="folder" />}
-    right={(props) => <IconButton {...props} icon="dots-vertical" onPress={() => {}} />}
-  />
+        title={hotel.name}
+        
+        //subtitle={description}
+        left={(props) => <Avatar.Icon {...props} icon="folder" />}
+        right={(props) => <IconButton {...props} icon="dots-vertical" onPress={() => {}} />}
+      />
         <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
       </Card>
 
       <Text>{hotel.name}</Text>
-      <Text>{hotel.description}</Text>
+      <Text>{description}</Text>
       <Button title="Book Now" onPress={handleBookNow} />
 
 
@@ -47,7 +61,3 @@ const style = StyleSheet.create({
 export default DetailsScreen;
 
 
-
-      {/* <Text style={{ fontSize: 24, marginBottom: 10 }}>
-        {user ? `Hello, ${user.userName}` : 'Welcome!'}
-      </Text> */}
