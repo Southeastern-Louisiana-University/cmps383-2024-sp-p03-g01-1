a0473f9fe4603873b889472374a3bdebcffb3f2e
Zachary Duncan <zachary.duncan@selu.edu>
2024-04-21 17:14:26 -0500

    Added images
    Added images to booking.js

--- Selu383.SP24.Mobile/features/booking.js
+++ Selu383.SP24.Mobile/features/booking.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, ScrollView, TouchableOpacity, checkInDate, checkOutDate } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, ScrollView, TouchableOpacity, Image } from 'react-native';
 import { Snackbar, Avatar, Card, IconButton, Checkbox } from 'react-native-paper';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import axios from 'axios';
 import { useAuth } from './AuthContext';
+import single from '../images/single.jpg';
+import double from '../images/doubles.jpg';
+import suite from '../images/suite.jpg';
 
 function BookingScreen({ route }) {
     const { hotel } = route.params;
@@ -16,13 +19,11 @@ function BookingScreen({ route }) {
     const [selectedRoom, setSelectedRoom] = useState(null); 
     const [availableRooms, setAvailableRooms] = useState([]);
     const [error, setError] = useState(null);
-    //const [checked, setChecked] = useState(false);
     const { hotelId } = route.params;
     const { user } = useAuth();
 
     useEffect(() => {
       fetchRooms(hotel.id); 
-      //console.log(hotelId)
     }, [hotel.id]);
 
     const fetchRooms = async (hotelId) => {
@@ -31,20 +32,13 @@ function BookingScreen({ route }) {
         const response = await axios.get(`https://selu383-sp24-p03-g01.azurewebsites.net/api/rooms?hotelId=${hotelId}`);
         console.log('Response data:', response.data);
         const filteredRooms = response.data.filter(room => room.hotelId === hotelId);
-        filteredRooms.forEach(room => {
-          console.log('Room Id:', room.id);
-        });        
         setAvailableRooms(filteredRooms);        
         setError(null); 
-        console.log('Available rooms:', availableRooms); 
-
       } catch (error) {
         console.error('Error fetching hotels:', error);
         setError('Failed to fetch hotels. Please try again.'); 
       }
     };
-
-    
 
     const handleRoomSelection = (room) => {
       if (selectedRoom && selectedRoom.id === room.id) {
@@ -52,21 +46,8 @@ function BookingScreen({ route }) {
       } else {
           setSelectedRoom(room);
       }
-      console.log('Selected Room:', room); // Log the current room being processed, not the state
+      console.log('Selected Room:', room); 
     };
-
-    // const handleRoomSelection = (room) => {
-    //   room.checked = !room.checked;
-    //   if (selectedRoom === room) {
-    //     setSelectedRoom(null);
-    //     setChecked(false); 
-    //   } else {
-    //     setSelectedRoom(room); 
-    //     setChecked(true); 
-    //   }
-    //   console.log('Selected Room:', selectedRoom);
-
-    // };
 
     const handleBookRoom = async () => {
       if (!selectedRoom) {
@@ -89,7 +70,6 @@ function BookingScreen({ route }) {
           return;
         }
         console.log('Selected Room ID:', selectedRoomId);
-        //console.log('Hotel ID:', hotel.id);
         const bookingData = {
           hotelId: hotel.id,
           roomId: selectedRoomId,
@@ -97,28 +77,23 @@ function BookingScreen({ route }) {
           checkInDate: checkInDate.toISOString(), 
           checkOutDate: checkOutDate.toISOString(),
         };
-      
-      console.log(bookingData)
 
-      const response = await axios.post(`https://selu383-sp24-p03-g01.azurewebsites.net/api/hotels/${hotel.id}/bookings`, bookingData);
+        const response = await axios.post(`https://selu383-sp24-p03-g01.azurewebsites.net/api/hotels/${hotel.id}/bookings`, bookingData);
 
-      console.log('Booking successful:', response.data);
-      //console.log('Booking Room:', selectedRoom.id);
-      //console.log('Booking info:', response.data);
+        console.log('Booking successful:', response.data);
 
-      setSnackbarMessage('Room booked successfully');
-      setShowSnackbar(true);
+        setSnackbarMessage('Room booked successfully');
+        setShowSnackbar(true);
 
- 
-      setCheckInDate('');
-      setCheckOutDate('');
-      setSelectedRoom(null);
+        setCheckInDate('');
+        setCheckOutDate('');
+        setSelectedRoom(null);
   
-    } catch (error) {
-      console.error('Error booking room:', error);
-      setSnackbarMessage('Failed to book room. Please try again.');
-      setShowSnackbar(true);
-    }
+      } catch (error) {
+        console.error('Error booking room:', error);
+        setSnackbarMessage('Failed to book room. Please try again.');
+        setShowSnackbar(true);
+      }
     };
   
     const handleCheckInDateSelection = () => {
@@ -137,25 +112,33 @@ function BookingScreen({ route }) {
           <TouchableOpacity key={room.id} onPress={() => handleRoomSelection(room)}>
 
             <Card>
-            <Card.Title
-              title={room.type}
-              left={(props) =>     
-                <Avatar.Icon
-                  {...props}
-                  icon={
-                  selectedRoom === room
-                    ? `bed-${room.type.toLowerCase()}`
-                    : `bed-${room.type.toLowerCase()}-outline`
-                }
-              />}
-              right={(props) =>     
-              <Checkbox
-                //status={checked ? 'checked' : 'unchecked'}
-                onPress={() => {
-                  handleRoomSelection(room);
-                }}
-              />}
-            /> 
+              <Card.Title
+                title={room.type}
+                left={(props) =>     
+                  <Avatar.Icon
+                    {...props}
+                    icon={
+                      selectedRoom === room
+                        ? `bed-${room.type.toLowerCase()}`
+                        : `bed-${room.type.toLowerCase()}-outline`
+                    }
+                  />}
+                right={(props) =>     
+                  <Checkbox
+                    onPress={() => {
+                      handleRoomSelection(room);
+                    }}
+                  />}
+              /> 
+<Image
+  source={
+    room.type === 'Single' ? single :
+    room.type === 'Double' ? double :
+    room.type === 'Suite' ? suite :
+    null
+  }
+  style={style.roomImage}
+/>
               <Card.Title
                 title={room.type}
                 subtitle={`Price: $${room.price}`}
@@ -172,28 +155,25 @@ function BookingScreen({ route }) {
         <Text style={style.description}>{hotel.description}</Text>
 
         <View style={style.dateContainer}>
-        <TouchableOpacity onPress={handleCheckInDateSelection}>
+          <TouchableOpacity onPress={handleCheckInDateSelection}>
             <Text style={style.dateLabel}>Check-in Date:</Text>
             <Text style={style.dateText}>{checkInDate ? new Date(checkInDate).toLocaleDateString() : 'Select Date'}</Text>
-        </TouchableOpacity>
+          </TouchableOpacity>
 
           {showCheckInDatePicker && (
-
-
-          <DateTimePicker
-            style={{ width: 200 }}
-            value={checkInDate ? new Date(checkInDate) : new Date()} 
-            mode="date"
-            display="spinner" 
-            onChange={(event, date) => {
-              if (date) {
-                setCheckInDate(date);
-                setShowCheckInDatePicker(false);
-              }
-            }} 
-          />
+            <DateTimePicker
+              style={{ width: 200 }}
+              value={checkInDate ? new Date(checkInDate) : new Date()} 
+              mode="date"
+              display="spinner" 
+              onChange={(event, date) => {
+                if (date) {
+                  setCheckInDate(date);
+                  setShowCheckInDatePicker(false);
+                }
+              }} 
+            />
           )}
-
 
           <TouchableOpacity onPress={handleCheckOutDateSelection}>
             <Text style={style.dateLabel}>Check-out Date:</Text>
@@ -216,10 +196,7 @@ function BookingScreen({ route }) {
           )}
         </View>
 
-
-
         <Button title="Book Room" onPress={handleBookRoom} />
-
 
         <Snackbar
           visible={showSnackbar}
@@ -230,45 +207,49 @@ function BookingScreen({ route }) {
         </Snackbar>
       </ScrollView>
     );
-  }
+}
   
-  const style = StyleSheet.create({
-    container: {
-        flexGrow: 1,
-        padding: 20,
-    },
-    hotelName: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginBottom: 10,
-        marginTop: 20,
-    },
-    description: {
-        marginBottom: 20,
-    },
-    input: {
-        marginBottom: 20,
-        paddingHorizontal: 10, 
-        backgroundColor: '#fff', 
-        borderRadius: 5, 
-        borderWidth: 1, 
-        borderColor: '#ccc', 
-    },
-    dateContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        marginBottom: 20,
-    },
-    dateLabel: {
-        fontSize: 16,
-        fontWeight: 'bold',
-    },
-    dateText: {
-        fontSize: 16,
-    },
-
+const style = StyleSheet.create({
+  container: {
+      flexGrow: 1,
+      padding: 20,
+  },
+  hotelName: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginBottom: 10,
+      marginTop: 20,
+  },
+  description: {
+      marginBottom: 20,
+  },
+  input: {
+      marginBottom: 20,
+      paddingHorizontal: 10, 
+      backgroundColor: '#fff', 
+      borderRadius: 5, 
+      borderWidth: 1, 
+      borderColor: '#ccc', 
+  },
+  dateContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      marginBottom: 20,
+  },
+  dateLabel: {
+      fontSize: 16,
+      fontWeight: 'bold',
+  },
+  dateText: {
+      fontSize: 16,
+  },
+  roomImage: {
+      width: '100%',
+      height: 200,
+      resizeMode: 'cover',
+      marginBottom: 10,
+  },
 });
 
-  export default BookingScreen;
-
+export default BookingScreen;
 
