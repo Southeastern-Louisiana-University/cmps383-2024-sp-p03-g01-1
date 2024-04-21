ddad6889f80c9aa4e07003aa62f01bbe0047b436
Sbabin01 <145293516+Sbabin01@users.noreply.github.com>
2024-04-20 20:02:36 -0500

    Update Navbar.tsx

--- Selu383.SP24.Web/src/Components/Navbar.tsx
+++ Selu383.SP24.Web/src/Components/Navbar.tsx
@@ -1,4 +1,4 @@
-import '../App.css'
+/*import '../App.css'
 
 import logo from "../images/logo.png";
 
@@ -60,4 +60,73 @@ function Navbar() {
   
   );
 }
+export default Navbar;*/
+
+import React, { useContext } from "react";
+import AuthContext from "../features/authentication/AuthContext";
+import logo from "../images/logo.png";
+import LogoutButton from "../features/authentication/logout";
+
+function Navbar() {
+  const authContext = useContext(AuthContext);
+  console.log("AuthContext:", authContext);
+  console.log("User:", authContext?.user);
+  
+  const handleClick = () => {
+  };
+
+  return (
+    <nav className="navbar-container">
+      <div className="navbar rounded-pill navbar-custom">
+        <div className="row row-custom">
+          <div className="col col-custom">
+            <img src={logo} className="image-custom rounded-pill" alt="Logo"/>
+          </div>
+          <div className="col nav-p-custom"></div>
+          <div className="nav-col-link-custom">
+            <a className="nav-link nav-link-custom" href="javascript:void(0)">Travel</a>
+          </div>
+          <div className="nav-col-link-custom">
+            <a className="nav-link nav-link-custom" href="javascript:void(0)">Offers</a>
+          </div>
+          <div className="nav-col-link-custom">
+            <a className="nav-link nav-link-custom" href="javascript:void(0)">About</a>
+          </div>
+          <div className="nav-col-link-custom">
+            <a className="nav-link nav-link-custom" href="javascript:void(0)">Contact</a>
+          </div>
+          <div className="nav-col-custom">
+            <button className="icon-button" onClick={handleClick}>
+              <i className="fas fa-phone icon-custom icon-rotate"></i>
+            </button>
+          </div>
+          <div className="nav-col-custom">
+            <button className="icon-button" onClick={handleClick}>
+              <i className="far fa-envelope icon-custom"></i>
+            </button>
+          </div>
+          <div className="nav-col-custom">
+            <button className="icon-button" onClick={handleClick}>
+              <i className="fas fa-map-marker-alt icon-custom"></i>
+            </button>
+          </div>
+          <div className="nav-col-custom">
+            <button className="icon-button" onClick={handleClick}>
+              <i className="fas fa-user-alt icon-custom"></i>
+            </button>
+          </div>
+          {authContext && authContext.user ? (
+            <LogoutButton>Logout</LogoutButton>
+          ) : (
+            <button className="btn rounded-pill nav-btn-custom" type="button" onClick={handleClick}>
+              Login
+            </button>
+          )}
+        </div>
+      </div>
+    </nav>
+    
+  );
+}
+
 export default Navbar;
